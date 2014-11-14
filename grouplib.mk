e1c668c2f27654f01d3d991421334d0b9ed9fd9b
Nick Clark <nicholas.clark@gmail.com>
2014-11-13 22:31:49 -0800

    Reorganized files.

--- grouplib.mk
+++ grouplib.mk
@@ -1,31 +1,50 @@
 #--------------- Grouplib: GNU Make Grouped-Target Library --------------------#
 #
-# Written by Nicholas Clark on 11-November-2014.
+# Version 2.0.
+#
+# Written by Nicholas Clark on 13-November-2014.
 # Released under the terms of the GNU General Public License version 2.
 #
 # Grouplib is a pure GNU Make library that provides a set of user functions
 # (which can be called with GNU Make's 'call' command) for managing 
-# multi-target recipes.
+# multi-target recipes. It uses auto-generated (and auto-deleted) sentinel
+# files along with a little bit of magic to provide parallel-safe grouped
+# outputs.
 #
 # Quick Refererence
+# -----------------
 #
 # Normal use cases:
 #
 # $(call group_create,groupname,group_deps,group_outputs)
-# $(call group,groupname)
+# $(call group_target,groupname)
 # $(call group_deps,groupname)
-# $(call group_finish,groupname)
 # 
+# $(call group_get_intermediates)
+# $(call group_get_phonies)
+#
 # Advanced use cases:
 #
 # $(call group_outputs,groupname)
 # $(call group_sentinel,groupname) 
-# $(call group_all_sentinels)
+# $(call group_get_sentinels)
 # $(call group_getdir)
 # $(call group_setdir,dirname)
-# $(call group_get_phonies)
 #
-##----------------------- GNU Make Version Checker ---------------------------#
+# Sample use:
+#
+#     $(call group_create,frobnicate,foo bar, baz bob)
+#     $(call group_target,frobnicate): $(call group_deps, frobnicate)
+#         cat foo bar > baz
+#         touch bob
+#
+#     corral: baz bob
+#	      touch corral
+#
+#     .INTERMEDIATE: $(call group_get_intermediates)
+#     .PHONY: $(call group_get_phonies)
+#
+#------------------------ GNU Make Version Checker ----------------------------#
 
 # Grouplib.mk works with version 3.82 of Make or higher. This checker
 # enforces the version.
@@ -46,55 +65,76 @@ $(warning GNU Make version 3.82 or higher is required)
 $(error (version [$(_sgMAJOR).$(_sgMINOR)] detected))
 endif
 
-#------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 
 # These variables are used internally. The sentinel storage directory
 # can also be manually set through the use of group_setdir, if the
 # default (.sentinels) isn't acceptable in your build.
 
-_SENTINEL_DIR := .sentinels
-_SENTINEL_LIST :=
+_sSENTINEL_DIR := .
+_sGROUP_LIST :=
 
-# _smissing accepts a list of input filenames, and returns a list of
-# any files that are missing (or an empty string otherwise)
+#---------------------------- Freshness Checker -------------------------------#
 
-_smissing = $(strip $(foreach x,$(1),$(if $(wildcard $(x)),,$(x))))
+# This routine is used to determine the freshness of a set of targets
+# in a sandbox environment. It is used within Grouplib to tell
+# internal targets when they need to be updated.
+#
+# It works by generating a small dummy Makefile, feeding it to Make, and
+# checking to see whether the targets are stale or not (returned via
+# the response from 'make -q').
 
-_ssentinel_name = $(_SENTINEL_DIR)/_$(1).sentinel
-_sdepends_name = _GROUP_$(1)_DEPENDS
-_soutputs_name = _GROUP_$(1)_OUTPUTS
-_sphony = _GROUP_$(1)_PHONY
+_supdate_needed = $(strip $(subst 0,,$(shell \
+printf "default: $(2)\n\n" > $(call _smakefile);\
+printf "$(2): $(1)\n\techo OLD\n" >> $(call _smakefile);\
+MAKEFLAGS="" MFLAGS="" GNUMAKEFLAGS="" \
+$(MAKE) --no-print-directory -q -f $(call _smakefile) 2>&1;\
+RES=$$?;\
+rm -f $(call _smakefile);\
+echo $$RES;)))
+
+#------------------------------ Other Commands --------------------------------#
+
+_ssentinel = $(_sSENTINEL_DIR)/_$(1).sentinel
+_smakefile = $(call _ssentinel,_smakefile).mk
+_sphony = _sGROUP_$(1)_PHONY
+_sdepends_name = _sGROUP_$(1)_DEPENDS
+_soutputs_name = _sGROUP_$(1)_OUTPUTS
 _sdepends = $(value $(call _sdepends_name,$(1)))
 _soutputs = $(value $(call _soutputs_name,$(1)))
-_sverify = $(if $(call _smissing,$(call _soutputs,$(1))),$(call _sphony,$(1)),)
 
 define _smkgroup_code =
-$(if $(filter $(1),$(_SENTINEL_LIST)),$(error group '$(1)' is already defined),)
-_SENTINEL_LIST := $(strip $(_SENTINEL_LIST) $(1))
+$(if $(filter $(1),$(_sGROUP_LIST)),$(error group '$(1)' is already defined),)
 $(if $(3),,$(error group '$(1)' outputs not defined))
 $(if $(2),,$(warning group '$(1)' sources not defined))
 $(if $(1),,$(error group-name not provided for $(3)))
-$(3): $(call _ssentinel_name,$(1))
-$(call _sphony,$(1)):
-
+_SRESULT :=  $(shell rm -f $(call _ssentinel,$(1)))
+_sGROUP_LIST := $(strip $(_sGROUP_LIST) $(1))
 $(call _sdepends_name,$(1)) := $(2)
 $(call _soutputs_name,$(1)) := $(3)
+
+$(3): $(call _ssentinel,$(1))
+	@$(foreach x,$(3),test -e $(x) &&) printf '';
+	@rm -f $(call _ssentinel,$(1))
+
+$(call _ssentinel,$(1)): \
+$(if $(call _supdate_needed,$(2),$(3)),$(call _sphony,$(1)),)
+	@touch $(call _ssentinel,$(1))
+
 endef
 
-define _sgroup_finish =
-@mkdir -p $(_SENTINEL_DIR)
-@touch $(call _ssentinel_name,$(1))
-endef
+#--------------------------- User-Facing Commands -----------------------------#
 
 group_create = $(eval $(call _smkgroup_code,$(1),$(2),$(3)))
-group = $(call _ssentinel_name,$(1))
-group_deps = $(call _sdepends,$(1)) $(call _sverify,$(1))
-group_finish = $(call _sgroup_finish,$(1))
+group_deps = $(call _sdepends,$(1))
+group_target = $(call _sphony,$(1))
 
 group_outputs = $(call _soutputs,$(1))
-group_sentinel = $(call _ssentinel_name,$(1))
+group_sentinel = $(call _ssentinel,$(1))
 
-group_all_sentinels = $(_SENTINEL_LIST)
-group_getdir = $(_SENTINEL_DIR)
-group_setdir = $(eval _SENTINEL_DIR := $(1))
-group_get_phonies = $(foreach x,$(_SENTINEL_LIST),$(call _sphony,$(x)))
+group_get_intermediates = $(call group_get_sentinels) $(call _smakefile)
+group_get_phonies = $(foreach x,$(_sGROUP_LIST),$(call _sphony,$(x)))
+
+group_get_sentinels = $(foreach x,$(_sGROUP_LIST),$(call _ssentinel,$(x)))
+group_getdir = $(_sSENTINEL_DIR)
+group_setdir = $(eval _sSENTINEL_DIR := $(1))
